ffda7284d85e9a8130c3c626fc84131e5f0d779e
gelicia <kdurivage@gelicia.com>
2013-03-03 22:41:22 -0600

    Add a slider to go over a year's data, add option to not include downtown west

--- js/main.js
+++ js/main.js
@@ -1,7 +1,100 @@
+/*
+things: 
+this gets a range of reds each month. every month is on a 1-100 scale, it does not make a scale by what all the months are
+
+
+to improve:
+option to leave out certain neighborhoods
+option to leave out certain crimes
+auto play
+
+*/
+
+//we only use the month and year of this date.
+var minYear = 2012;
+var minMonth = 0;
+
+var dateOfInterest = new Date(minYear, minMonth, 1);
+
+var regex = /[\s\.]/g;
+
+var monthNames = [ "January", "February", "March", "April", "May", "June",
+    "July", "August", "September", "October", "November", "December" ];
+
+function offsetToMonth(offset){
+	dateOfInterest.setYear(minYear + Math.floor(offset/12));
+	dateOfInterest.setMonth(minMonth + (offset%12));
+	return monthNames[dateOfInterest.getMonth()] + " " + dateOfInterest.getFullYear();
+}
+
+function dateToFileName(){
+	var monthStr = String(dateOfInterest.getMonth() + 1);
+	monthStr = (monthStr.length == 1 ? '0' + monthStr : monthStr);
+	return String(dateOfInterest.getFullYear()) + monthStr;
+}
+
+function redraw(){
+		var noDowntownWest = $(noDW).attr('checked') === undefined ? false : true;
+
+		var ds = new Miso.Dataset({
+		url : "data/" + dateToFileName() + ".csv",
+		delimiter : ","
+		});
+
+		_.when(
+			ds.fetch({
+			success : function() {
+				var thisMax = this.where({ 
+					rows : function(row) {
+						return !noDowntownWest || row.NEIGHBORHOOD !== 'DOWNTOWN WEST';
+					}
+				}).max('Total ');
+
+				//color range between white and red
+				var colorRange = d3.scale.linear().domain([0, thisMax]).range(["#FFFFFF", "#FF0000"]);
+
+				this.each(
+					function(row){
+						//console.log(row.NEIGHBORHOOD.replace(regex, "").toLowerCase() + " = " + row["Total "]);
+						if (row.NEIGHBORHOOD !== null && (!noDowntownWest || row.NEIGHBORHOOD !== 'DOWNTOWN WEST')){
+							d3.select("#" + (row.NEIGHBORHOOD).replace(regex, "").toLowerCase())
+								.attr({"dataDate" : dateToFileName()})
+								.transition()
+								.duration(1000)
+								.ease("linear")
+								.attr({
+									fill : colorRange(row["Total "])
+								});
+						}
+						
+					}
+				);
+			}
+			})
+		).then(
+			function(){
+				//I couldn't get this to work with a pure selector
+				var allPaths = d3.selectAll("svg g path");
+
+				allPaths.each(function(){
+					var thisPath = d3.select(this);
+
+					if (thisPath.attr("dataDate") != dateToFileName() || (noDowntownWest && thisPath.attr("id") == 'downtownwest')){
+						thisPath
+							.transition()
+							.duration(1000)
+							.ease("linear")
+							.attr("fill", "#666666");
+					}
+				});
+			}
+		);
+}
+
 function loadDataset(){
 	var width = 810;
 	var height = 600;
-	var regex = /[\s\.]/g;
+	
 	//so many magic numbers...TODO
 	var path = d3.geo.path().projection(
 		d3.geo.mercator().center([-93.26144099999999, 44.970675]).
@@ -15,7 +108,6 @@ function loadDataset(){
 	})
 	.append("g");
 
-	// Sorry about the stupid color names
    d3.json("data/minneapolis.geojson", function(collection) {
 		neigh
 			.selectAll("path")
@@ -25,41 +117,16 @@ function loadDataset(){
 			.attr(
 				{
 					"d" : path,
-					fill : "grey",
-					stroke : "black",
-					id : function(d) {return (d.properties.name).replace(regex, "").toLowerCase();}
-				})
-				.on("click", function(d) {
-					console.log((d.properties.name).replace(regex, "").toLowerCase());
-				});
+					fill : "#666666", //Init at gray to show neighborhoods not matched up
+					stroke : "#000000",
+					id : function(d) {return (d.properties.name).replace(regex, "").toLowerCase();},
+					"dataDate" : "00"
+				}
+			)
+			.on("click", function(d) {
+				console.log((d.properties.name).replace(regex, "").toLowerCase());
+			});
 	});
 
-   var ds = new Miso.Dataset({
-		url : "data/201202.csv",
-		delimiter : ","
-		});
-
-		ds.fetch({
-			success : function() {
-				var thisMax = this.max('Total ');
-				var colorRange = d3.scale.linear().domain([0, thisMax]).range(["white", "red"]);
-
-				this.each(
-					function(row){
-						//console.log(row.NEIGHBORHOOD.replace(regex, "").toLowerCase() + " = " + row["Total "]);
-						if (row.NEIGHBORHOOD !== null){
-							d3.select("#" + (row.NEIGHBORHOOD).replace(regex, "").toLowerCase())
-								.attr({
-									fill : colorRange(row["Total "])
-								})
-								.on("click", function(d) {
-									console.log((d.properties.name).replace(regex, "").toLowerCase() + " " + row["Total "]);
-								});
-						}
-						
-					}
-				);
-			}
-		});
-
+   redraw();
 }
